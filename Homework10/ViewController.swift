80e2bcee984c887e907ffbbe897602db37f7b5b4
m01otov <67689912+m01otov@users.noreply.github.com>
2021-09-12 11:34:59 +0500

    Initial Commit

---
+++ Homework10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework10
+//
+//  Created by Ilya Melnikov on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
